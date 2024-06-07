341eedb74c317cb87d563d00e7edeeea377da449
Anastasia Belosludceva <anastasia.tsyganova2017@yandex.ru>
2024-06-07 23:48:38 +0500

    Дообавлен js

---
+++ threads/login.js
@@ -0,0 +1,8 @@
+const lolBox = document.querySelector("#box-lol");
+lolBox.addEventListener("click",()=>{
+    alert("lol");
+});
+const kekBox = document.querySelector("#box-kek");
+kekBox.addEventListener("click",()=>{
+    alert("kek");
+});
