9e7f40311ed6ffac7d02b25f699fcece2849a6b3
Anastasia Belosludceva <anastasia.tsyganova2017@yandex.ru>
2024-06-08 00:04:39 +0500

    Добавили клик

--- threads/login.js
+++ threads/login.js
@@ -1,8 +1,22 @@
-const lolBox = document.querySelector("#box-lol");
-lolBox.addEventListener("click",()=>{
+document.querySelector("#box-lol").addEventListener("click",()=>{
     alert("lol");
 });
 const kekBox = document.querySelector("#box-kek");
 kekBox.addEventListener("click",()=>{
     alert("kek");
 });
+
+[
+    {
+        id: "#box-lol",
+        text: "lol",
+    },
+    {
+        id: "#box-kek",
+        text: "kek",
+    },
+].forEach((e)=>{
+    document.querySelector(e.id).addEventListener("click",()=>{
+        alert(e.text);
+    });;
+})
