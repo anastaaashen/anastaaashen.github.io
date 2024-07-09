1cdbc330c279b43f6f069140e9e85bef5b99bfd5
Anastasia Belosludceva <anastasia.tsyganova2017@yandex.ru>
2024-07-09 14:33:46 +0500

    Добавлен header

--- src/pages/threads-home.tsx
+++ src/pages/threads-home.tsx
@@ -1,13 +1,34 @@
 import iglogo from '../threads-logo.png';
+import styles from './threads-home.module.css';
+import home from '../home.png';
+import human from '../human.png';
+import like from '../like.png';
+import search from '../search.png';
+import post from '../post.png';
+
 
 export function ThreadsHome () {
 return (
-    <div> 
-        <img src={iglogo} alt="иконка инстаграм"></img> 
-    </div>
 
+    <header>
+    <nav className={styles.container}>
+        <div className={styles.navbar}>
+
+        <img src={iglogo} alt="logo"></img>
+
+            <ul>
+                <button className={styles.btn} ><img src={home} alt="home"></img></button>
+                <button className={styles.btn}><img src={search} alt="home"></img></button>
+                <button className={styles.btn}><img src={post} alt="home"></img></button>
+                <button className={styles.btn}><img src={like} alt="home"></img></button>
+                <button className={styles.btn}><img src={human} alt="home"></img></button>
+            </ul>
+
+            <div className={styles.login}>
+                Log in
+            </div>
+        </div>
+    </nav>
+    </header>
 )
-
-
-
 }
