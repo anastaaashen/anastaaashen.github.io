ef29a5c9201fa11dd82da9a791d7dc32d2eb4429
Anastasia Belosludceva <anastasia.tsyganova2017@yandex.ru>
2024-07-31 23:19:10 +0500

    Правки по вопросам

--- src/pages/components/content/content.tsx
+++ src/pages/components/content/content.tsx
@@ -1,31 +1,37 @@
 import styles from './content.module.css';
-import like from './like2.png'
-import comment from './comment.png'
-import repost from './repost.png'
-import share from './share.png'
-import foto from './avatar.jpeg'
-import content from './nature.jpg'
+import like from './like2.png';
+import comment from './comment.png';
+import repost from './repost.png';
+import share from './share.png';
+import foto from './avatar.jpeg';
+import content from './nature.jpg';
+import { ReactComponent as DotsIcon } from './dots.svg';
 
 export const Content = () => {
     return (
         <div className={styles.container}>
             <div className={styles.blok}>
-                <div className={styles.header}>
-                   <div className={styles.userlogo}></div>
-                     <div className={styles.user}>
-                        <div className={styles.username}>user1</div>
-                        <div className={styles.date}>2 d.</div>
-                     </div>
-                        <div className={styles.burger}>...</div>
-                     
+                <div>
+                    <div className={styles.userlogo}></div>
                 </div>
-                <div className={styles.post}>It's a good weather today</div>
-                <div className={styles.content}> <img src={foto}></img></div>
-                <div className={styles.communication}>
-                    <button className={styles.btn}><img src={like} alt="like"></img></button>
-                    <button className={styles.btn}><img src={comment} alt="comments"></img></button>
-                    <button className={styles.btn}><img src={repost} alt="repost"></img></button>
-                    <button className={styles.btn}><img src={share} alt="share"></img></button>
+                <div>
+                    <div className={styles.header}>
+                        <div className={styles.user}>
+                            <div className={styles.username}>user1</div>
+                            <div className={styles.date}>2 d.</div>
+                        </div>
+                        <div className={styles.burger}>
+                            <DotsIcon />
+                        </div>
+                    </div>
+                    <div className={styles.post}>It's a good weather today</div>
+                    <div className={styles.content}> <img src={foto}></img></div>
+                    <div className={styles.communication}>
+                        <button className={styles.btn}><img src={like} alt="like"></img></button>
+                        <button className={styles.btn}><img src={comment} alt="comments"></img></button>
+                        <button className={styles.btn}><img src={repost} alt="repost"></img></button>
+                        <button className={styles.btn}><img src={share} alt="share"></img></button>
+                    </div>
                 </div>
             </div>
             <div className={styles.blok}>
@@ -39,7 +45,7 @@ export const Content = () => {
                      
                 </div>
                 <div className={styles.post}>It's a good weather today</div>
-                <div className={styles.content}> <img src={content}></img></div>
+                <div className={styles.content}> <img src={content }></img></div>
                 <div className={styles.communication}>
                     <button className={styles.btn}><img src={like} alt="like"></img></button>
                     <button className={styles.btn}><img src={comment} alt="comments"></img></button>
